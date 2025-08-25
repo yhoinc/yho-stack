123299dc1d00cf29695f48169660af73fb3aea80
yhoinc <info@yhoinc.com>
2025-08-24 22:50:26 -0500

    Add files via upload

---
+++ web/next.config.ts
@@ -0,0 +1,13 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://localhost:8000/:path*", // FastAPI dev server
+            },
+        ];
+    },
+};
+
+module.exports = nextConfig;
